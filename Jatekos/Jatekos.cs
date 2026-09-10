d9ceca032a56e8d9392550cd870c4701cd0182fb
Marok07 <karsai.marko.2022d@telekimezotur.hu>
2026-09-10 11:47:24 +0200

    4. feladat

---
+++ Jatekos/Jatekos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jatekosok
+{
+    internal class Jatekos
+    {
+        private int elet;
+        public string Nev { get; set; }
+        public int Elet
+        {
+            get;
+            set {
+                if (value < 0) elet = 0;
+                else if (value > 100) elet = 100;
+                else elet = value;
+                }
+        }
+    }
+}
---
+++ Jatekos/Program.cs
@@ -0,0 +1,11 @@
+﻿using Jatekosok;
+
+Jatekos jatekos = new Jatekos();
+jatekos.Nev = "Nugat";
+jatekos.Elet = 120;
+Console.WriteLine($"Név: {jatekos.Nev}, Élet: {jatekos.Elet}");
+Jatekos jatekos2 = new Jatekos();
+jatekos2.Nev = "Logat";
+jatekos2.Elet = -20;
+Console.WriteLine($"Név: {jatekos2.Nev}, Élet: {jatekos2.Elet}");
+
