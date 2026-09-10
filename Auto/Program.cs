d1292368e878de7f95cdc6ba89d1d49a5bf614d0
Marok07 <karsai.marko.2022d@telekimezotur.hu>
2026-09-10 10:15:53 +0200

    1. feladat

--- Auto/Program.cs
+++ Auto/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.Java;
+
+class Auto
+{
+    public string Marka;
+    public int Eletkor;
+
+    public Auto(string marka, int eletkor)
+    {
+        Marka = marka;
+        Eletkor = eletkor;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Auto auto1 = new Auto("Mercedes-Benz", 27);
+        Auto auto2 = new Auto("Honda", 3);
+        Console.WriteLine($"Márka: {auto1.Marka}, Életkor: {auto1.Eletkor}");
+        Console.WriteLine($"Márka: {auto2.Marka}, Életkor: {auto2.Eletkor}");
+    }
+}
