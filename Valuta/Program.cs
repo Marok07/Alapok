f49156180e3c35bd7958a315278115576691411c
Marok07 <karsai.marko.2022d@telekimezotur.hu>
2026-09-10 10:54:53 +0200

    2. feladat

---
+++ Valuta/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using Valuta;
+
+var mai = new ValutaValto(360.3d);
+Console.WriteLine($"3,5 Euró 360,3 Forintos árfolyammal számolva: {mai.EuroToHuf(3.5d)} Forint");
+
---
+++ Valuta/ValutaValto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valuta
+{
+    internal class ValutaValto
+    {
+        public double Arfolyam;
+
+        public ValutaValto(double Arfolyam)
+        {
+            this.Arfolyam = Arfolyam;
+        }
+        public double EuroToHuf(double euro)
+        {
+            return Arfolyam* euro;
+        }
+        public static double FixEuroToHuf(double euro)
+        {
+            return euro*400;
+        }
+    }
+}
